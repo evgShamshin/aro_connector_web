2b68136765019d34e55e4a1a4fff89250a1a423e
evg_shamshin <i@eshamshin.ru>
2025-03-11 23:02:58 +0300

    2025.03.11

--- website_project/website_app/views.py
+++ website_project/website_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 
 # Create your views here.
-def index(request):  # HttpResponse
+def index(request: HttpRequest):  # HttpResponse
     return HttpResponse('Главная страница сайта')
 
 
-def categories(request):
+def categories(request: HttpRequest) -> HttpResponse:
     return HttpResponse('Категории сайта')
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('О сайте')
