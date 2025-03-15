15e8ac489de61c93872158d62fc3c1544f843140
evg_shamsin <i@eshamshin.ru>
2025-03-15 23:59:10 +0300

    2025.03.15

--- website_project/website_app/urls.py
+++ website_project/website_app/urls.py
@@ -5,10 +5,10 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
-    path('', views.by_index),
-    path('cats/', views.by_categories),
-    path('about/', views.by_about),
+    path('', views.by_index, name='home'),
+    path('cats/', views.by_categories, name='cats'),
+    path('about/', views.by_about, name='about'),
     path('slug/<slug:par_slug>/', views.by_slug, name='par_slug'),
     path('id/<int:par_id>/', views.by_id, name='par_id'),
-    path('archive/<year4:year>/', views.by_archive),
+    path('archive/<year4:year>/', views.by_archive, name='archive'),
 ]
