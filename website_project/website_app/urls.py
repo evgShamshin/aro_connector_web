7042ba83a84ec27359954d9776dd734b49006ad0
evg_shamshin <i@eshamshin.ru>
2025-03-20 09:35:36 +0300

    2025.03.20

--- website_project/website_app/urls.py
+++ website_project/website_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, register_converter
 from . import views
-from . import converters
 
 # register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.main_page, name='home'),
-    path('category/<int:par_id>/', views.category_page, name='cats'),
+    path('category/<int:par_id>/', views.category_page, name='category'),
+    path('project/', views.project_page, name='project'),
+    path('services/', views.services, name='services'),
+    path('faq/', views.faq, name='faq'),
+    path('contact/', views.contact, name='contact'),
+    path('authorization/', views.authorization, name='authorization'),
 ]
--- website_project/website_app/views.py
+++ website_project/website_app/views.py
@@ -6,15 +6,40 @@ from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404
 def main_page(request: HttpRequest):  # HttpResponse
     # return HttpResponse('Главная страница сайта')
     data = {'title': 'Главная страница сайта',
-            'about': ["Проекты", "Услуги", "FAQ", "О компании"],
+            'about': [{'title': 'Проекты', 'title_url': 'project'},
+                      {'title': 'Услуги', 'title_url': 'services'},
+                      {'title': 'FAQ', 'title_url': 'faq'},
+                      {'title': 'Контакты', 'title_url': 'contact'},
+                      {'title': 'Войти', 'title_url': 'authorization'}, ],
             'payment': [['Помощь сайту:', 'Ссылка'], ['Номер карты:', '1234567890098765']],
-            'is_draft': False,
+            'cats': [{'title': 'Заполненность параметров', 'par_id': 1},
+                     {'title': 'Проверка координат', 'par_id': 2}, ],
             }
     return render(request, 'website_app/main.html', context=data)
 
 
 def category_page(request: HttpRequest, par_id: int) -> HttpResponse:
     return HttpResponse(f'Отображение команды с номером {par_id}')
+
+
+def project_page(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Проекты')
+
+
+def services(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Услуги')
+
+
+def faq(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Часто задаваемые вопросы')
+
+
+def contact(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Контакты')
+
+
+def authorization(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Авторизация')
 
 
 def page_nof_found(request, exception) -> HttpResponseNotFound:
