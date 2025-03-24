9eb3e84c5098495dcf7d874d8737eb17138a8a0d
evg_shamshin <i@eshamshin.ru>
2025-03-24 23:54:01 +0300

    2025.03.20

--- website_project/website_project/settings.py
+++ website_project/website_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2qu9(7-8ho35c9hit*e=3rsijrl@o8v6-6_%qte&(6277s^sb6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
