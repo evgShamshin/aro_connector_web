476acd718deb680a46c9b3331711a745f51b198b
evg_shamshin <i@eshamshin.ru>
2025-03-31 22:33:33 +0300

    2025.03.30

--- website_project/website_app/models.py
+++ website_project/website_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class WebsiteApp(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    link = models.URLField(blank=True)
+    # time_create = models.DateTimeField(auto_now_add=True)
+    # time_update = models.DateTimeField(auto_now=True)
+    # is_published = models.BooleanField(default=True)
