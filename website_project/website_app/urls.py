d6d0c1930ee71c8838d37c521bad13d9a86ffd3f
evg_shamshin <i@eshamshin.ru>
2025-04-09 23:57:00 +0300

    2025.04.09

--- website_project/website_app/urls.py
+++ website_project/website_app/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, register_converter
+from django.shortcuts import redirect
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.main_page, name='main'),
+    path('', lambda request: redirect('/connector/')),
     path('connector/', views.connector_page, name='connector'),
     path('connector/<slug:article_slug>/', views.connector_commands_page, name='connector_commands'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- website_project/website_app/views.py
+++ website_project/website_app/views.py
@@ -3,18 +3,16 @@ from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
 from .models import WebsiteApp
 
 
-def main_page(request: HttpRequest):  # HttpResponse
+def connector_page(request: HttpRequest):  # HttpResponse
     about = [{'title': 'BIM', 'title_url': 'https://www.arogroup.ru/bim '},
              {'title': 'IT', 'title_url': 'https://www.arogroup.ru/it'},
              {'title': 'Design', 'title_url': 'https://www.arogroup.ru/design'},
              {'title': 'Projects', 'title_url': 'https://www.arogroup.ru/projects'},
-             {'title': 'Connector', 'title_url': 'https://www.arogroup.ru/projects'},]
+             {'title': 'Connector', 'title_url': ''},]
 
-    payment = [['telegram:', 'newarogroup'], ['Номер карты:', '1234567890098765']]
     cats = WebsiteApp.objects.all()
     data = {'title': 'Набор команд',
             'about': about,
-            'payment': payment,
             'cats': cats}
     return render(request, 'website_app/main.html', context=data)
 
@@ -22,10 +20,6 @@ def main_page(request: HttpRequest):  # HttpResponse
 def connector_commands_page(request: HttpRequest, article_slug) -> HttpResponse:
     obj = get_object_or_404(WebsiteApp, article_slug=article_slug)
     return render(request, 'website_app/command.html', context={'obj': obj})
-
-
-def connector_page(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Connector")
 
 
 def nof_found_page(request, exception) -> HttpResponseNotFound:
