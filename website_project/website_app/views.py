a5a1539a56872e8851df4614d53284bd5b04127b
evg_shamshin <i@eshamshin.ru>
2025-04-10 23:58:22 +0300

    2025.04.10

--- website_project/website_app/views.py
+++ website_project/website_app/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
 from .models import WebsiteApp
 
+about = [{'title': 'BIM', 'title_url': 'https://www.arogroup.ru/bim '},
+         {'title': 'IT', 'title_url': 'https://www.arogroup.ru/it'},
+         {'title': 'Design', 'title_url': 'https://www.arogroup.ru/design'},
+         {'title': 'Projects', 'title_url': 'https://www.arogroup.ru/projects'},
+         {'title': 'Connector', 'title_url': 'connector'}, ]
+
 
 def connector_page(request: HttpRequest):  # HttpResponse
-    about = [{'title': 'BIM', 'title_url': 'https://www.arogroup.ru/bim '},
-             {'title': 'IT', 'title_url': 'https://www.arogroup.ru/it'},
-             {'title': 'Design', 'title_url': 'https://www.arogroup.ru/design'},
-             {'title': 'Projects', 'title_url': 'https://www.arogroup.ru/projects'},
-             {'title': 'Connector', 'title_url': ''},]
-
     cats = WebsiteApp.objects.all()
     data = {'title': 'Набор команд',
             'about': about,
@@ -19,7 +19,7 @@ def connector_page(request: HttpRequest):  # HttpResponse
 
 def connector_commands_page(request: HttpRequest, article_slug) -> HttpResponse:
     obj = get_object_or_404(WebsiteApp, article_slug=article_slug)
-    return render(request, 'website_app/command.html', context={'obj': obj})
+    return render(request, 'website_app/command.html', context={'obj': obj, 'about': about, })
 
 
 def nof_found_page(request, exception) -> HttpResponseNotFound:
