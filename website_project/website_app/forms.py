91c4af71067f6558b33311be2ba656de2279cd2c
evg_shamshin <i@eshamshin.ru>
2025-05-14 23:51:33 +0300

    2025.05.14

--- website_project/website_app/forms.py
+++ website_project/website_app/forms.py
@@ -9,10 +9,3 @@ class ConsultForm(forms.Form):
     description = forms.CharField(widget=forms.Textarea)
     date = forms.DateField(widget=forms.DateInput, required=False)
     attachment = forms.FileField(required=False)
-
-    fio = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    city = forms.CharField(max_length=100, required=False)
-    phone = forms.CharField(max_length=12, required=False)
-    agree = forms.BooleanField(required=False)
-    content = forms.CharField(widget=forms.Textarea)
