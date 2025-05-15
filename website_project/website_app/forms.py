212ba48158d65e3fb90941b1f60b411350cb5604
evg_shamshin <i@eshamshin.ru>
2025-05-15 23:53:12 +0300

    2025.05.14

--- website_project/website_app/forms.py
+++ website_project/website_app/forms.py
@@ -3,9 +3,9 @@ from .models import Consult
 
 
 class ConsultForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    surname = forms.CharField(max_length=255)
-    email = forms.EmailField()
-    description = forms.CharField(widget=forms.Textarea)
-    date = forms.DateField(widget=forms.DateInput, required=False)
-    attachment = forms.FileField(required=False)
+    name = forms.CharField(max_length=255, label="Имя")
+    surname = forms.CharField(max_length=255, label="Фамилия")
+    email = forms.EmailField(label="Электронная почта")
+    description = forms.CharField(widget=forms.Textarea, label="Описание")
+    date = forms.DateField(widget=forms.DateInput, required=False, label="Дата")
+    attachment = forms.FileField(required=False, label="Вложения")
