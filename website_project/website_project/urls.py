c390b77d80fd4e9a9319ca13584be536ee0e08dc
evg_shamshin <i@eshamshin.ru>
2025-06-08 10:27:42 +0300

    2025.06.08

--- website_project/website_project/urls.py
+++ website_project/website_project/urls.py
@@ -1,32 +1,13 @@
-"""
-URL configuration for website_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from website_app import urls as website_app_urls
+from website_users import urls as website_users_urls
 
-from website_app.views import nof_found_page
-
-\
-
-
-urlpatterns = {
+urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('connector/admin/', admin.site.urls),
-    path('', include('website_app.urls')),
-    path('users/', include('website_users.urls', namespace='users')), }
+    path('', include(website_app_urls)),
+    path('connector/users/', include(website_users_urls, namespace='users')), ]
 
 admin.site.site_header = 'Администрирование сайта'
 admin.site.index_title = 'Команды плагина ARO'
