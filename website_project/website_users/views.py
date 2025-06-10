e3ba0b2dc15a441ed24129905f94d4916944f0e9
evg_shamshin <i@eshamshin.ru>
2025-06-10 23:14:41 +0300

    2025.06.10

--- website_project/website_users/views.py
+++ website_project/website_users/views.py
@@ -1,9 +1,40 @@
-from django.http import HttpResponse
+from audioop import reverse
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+from website_app.models import About, Group, Tag
+from website_users.forms import LoginUserForm
 
 
 def login_user(request):
-    return HttpResponse("<h1>Welcome to BalakiRev!</h1>")
+    data = {'title': 'ARO Group',
+            'descr': """Плагин Connector - расширение для архитекторов и дизайнеров,
+                                добавляющее возможности в Autodesk Revit.
+                                Автоматизирует многие процессы и существенно сокращает время
+                                создания модели.
+                                Плагин совместим с 2019, 2020, 2021, 2022, 2023, 2024, 2025
+                                версиями Autodesk Revit.""",
+            'about': About.objects.all(),
+            'groups': Group.objects.all(),
+            'tags': Tag.objects.all(), }
+
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'],
+                                password=cd['password'])
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponse()
+    else:
+        form = LoginUserForm()
+
+    return render(request, 'users/login.html', context={'form': form})
 
 
 def logout_user(request):
-    return HttpResponse("<h1>Bye from BalakiRev!</h1>")
+    logout(request)
+    return HttpResponse()
