e86d727efcfd86782635e1aadcc6d6da9d86ea3a
evg_shamshin <i@eshamshin.ru>
2025-06-19 23:59:24 +0300

    2025.06.19

--- website_project/website_users/urls.py
+++ website_project/website_users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.LoginPageUser.as_view(), name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
