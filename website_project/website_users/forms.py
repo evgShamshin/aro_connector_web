905adc77d07a07f153a542806e30c0514d57f6a9
evg_shamshin <i@eshamshin.ru>
2025-06-21 23:57:13 +0300

    2025.06.20

--- website_project/website_users/forms.py
+++ website_project/website_users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class LoginUserForm(AuthenticationForm):
@@ -10,7 +10,7 @@ class LoginUserForm(AuthenticationForm):
                                widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
 
-class RegisterUserForm(forms.ModelForm):
+class RegisterUserForm(UserCreationForm):
     username = forms.CharField(label="Логин", widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
@@ -24,27 +24,8 @@ class RegisterUserForm(forms.ModelForm):
             'last_name': 'Фамилия'
         }
 
-        def clean_password2(self):
-            cd = self.cleaned_data
-            if cd['password1'] != cd['password2']:
-                raise forms.ValidationError("Пароли не совпадают!")
-            return cd['password']
-
         def clean_email(self):
             email = self.cleaned_data['email']
             if get_user_model().objects.filter(email=email).exists():
                 raise forms.ValidationError("Такой E-mail уже существует!")
             return email
-
-
-class LectorRegisterForm(forms.ModelForm):
-    username = forms.CharField(label="Логин", max_length=50)
-    password = forms.CharField(label="Пароль", max_length=30, widget=forms.PasswordInput())
-    password2 = forms.CharField(label="Повтор пароля", max_length=30, widget=forms.PasswordInput())
-
-    class Meta:
-        model = get_user_model()
-        fields = ["username", "email", "password", "password2"]
-        labels = {
-            'email': 'E-Mail'
-        }
